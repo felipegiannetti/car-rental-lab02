0f6b6e4a338da6954d25436ee92463e67a520bbf
Felipe Giannetti <felipegiannettifontenelle@gmail.com>
2026-04-17 21:38:20 -0300

    .

--- frontend/src/api/http.js
+++ frontend/src/api/http.js
@@ -2,7 +2,7 @@ import axios from 'axios'
 import { AUTH_STORAGE_KEY } from '../constants/auth'
 
 const http = axios.create({
-  baseURL: '/api',
+  baseURL: (import.meta.env.VITE_API_URL ?? '') + '/api',
   headers: { 'Content-Type': 'application/json' },
 })
 
